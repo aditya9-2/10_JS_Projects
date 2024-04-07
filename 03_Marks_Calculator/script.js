b405e1fa1d0ee8c8b886362caef0332528f419f1
Aditya <adityabasak7031@gmail.com>
2024-04-07 18:43:42 +0530

    commit

--- 03_Marks_Calculator/script.js
+++ 03_Marks_Calculator/script.js
@@ -1,36 +1,19 @@
+const calculateFormEl = document.getElementById("calculateForm");
+const resultEl = document.getElementById("result");
 
-const calculateFrom = document.getElementById('calculateForm');
-const submit = document.getElementById('btn');
-const result = document.getElementById('result');
-
-submit.addEventListener('click', (e) => {
-
+const calculateMarks = (event) => {
     const maxMarks = 400;
+    event.preventDefault();
 
-    e.preventDefault();
-    const formData = new FormData(calculateFrom);
+    const formData = new FormData(calculateFormEl);
+
     const data = {};
 
     formData.forEach((value, key) => {
         data[key] = +value;
-
     });
-
-    if (data.math < 0 || data.math > 100 || data.english < 0 || data.english > 100 || data.physics < 0 || data.physics > 100 || data.chemistry < 0 || data.chemistry > 100) {
-        alert("Please enter valid values between 0 and 100.");
-
-    }
-    else {
-
-        const totalMarks = data.math + data.english + data.physics + data.chemistry;
-
-        const percentage = (totalMarks / maxMarks) * 100;
-        result.innerText = `You have got ${totalMarks} out of ${maxMarks} and your percentage is - ${percentage}%`;
-    }
-
-});
-
-
-
-
+    const totalMarks = data.maths + data.science + data.hindi + data.english;
+    const percentage = Math.round((totalMarks / maxMarks) * 100);
+    resultEl.innerText = `You have got ${totalMarks} marks out of ${maxMarks} and your percentage is ${percentage}%`;
+};
 
